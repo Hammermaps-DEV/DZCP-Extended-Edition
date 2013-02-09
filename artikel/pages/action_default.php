68bb5ff186cfb77ca6fb9150ee23c378f6ae6df6
Lucas <godkiller_nt@hammermaps.de>
2013-02-09 14:08:54 +0100

    Update Artikel Default Page
    Code CleanUp,
    Remove unused pholdervars,
    Use Global lang_ Tag for HTML

--- artikel/pages/action_default.php
+++ artikel/pages/action_default.php
@@ -6,8 +6,13 @@
  * @link: http://www.dzcp.de || http://www.hammermaps.de
  */
 
+#############################################
+##### Code for 'DZCP - Extended Edition #####
+###### DZCP - Extended Edition >= 1.0 #######
+#############################################
+
 ####################################
-## Wird in einer Index ausgefÃ¼hrt ##
+## Wird in einer Index ausgeführt ##
 ####################################
 if (!defined('IS_DZCP'))
     exit();
@@ -16,48 +21,24 @@ if (_version < '1.0') //Mindest Version pruefen
     $index = _version_for_page_outofdate;
 else
 {
-  if(isset($_GET['page'])) $page = $_GET['page'];
-  else $page = 1;
+    $martikel = config('m_artikel');
+    $qry = db("SELECT * FROM ".$db['artikel']." WHERE public = 1 ORDER BY datum DESC LIMIT ".($page - 1)*$martikel.",".$martikel."");
+    $entrys = cnt($db['artikel']);
 
-  $qry = db("SELECT * FROM ".$db['artikel']."
-             WHERE public = 1
-             ORDER BY datum DESC
-             LIMIT ".($page - 1)*$martikel.",".$martikel."");
-
-  $entrys = cnt($db['artikel']);
-  if(_rows($qry))
-  {
-    while($get = _fetch($qry))
+    if(_rows($qry))
     {
-      $titel = '<a style="display:block" href="?action=show&amp;id='.$get['id'].'">'.$get['titel'].'</a>';
-
-      $class = ($color % 2) ? "contentMainSecond" : "contentMainFirst"; $color++;
-
-      $show .= show($dir."/artikel_show", array("titel" => $titel,
-                                                "kat" => $kat,
-                                                "id" => $get['id'],
-                                                "display" => "none",
-                                                "nautor" => _autor,
-                                                "ndatum" => _datum,
-                                                "class" => $class,
-                                                "ncomments" => _news_kommentare.":",
-                                                "viewed" => $viewed,
-                                                "text" => bbcode($get['text']),
-                                                "datum" => date("d.m.Y", $get['datum']),
-                                                "links" => $links,
-                                                "autor" => autor($get['autor'])));
+        $color = 1; $show = '';
+        while($get = _fetch($qry))
+        {
+            $titel = '<a style="display:block" href="?action=show&amp;id='.$get['id'].'">'.$get['titel'].'</a>';
+            $class = ($color % 2) ? "contentMainSecond" : "contentMainFirst"; $color++;
+            $show .= show($dir."/artikel_show", array("titel" => $titel, "class" => $class, "datum" => date("d.m.Y", $get['datum']), "autor" => autor($get['autor'])));
+        } //while end
     }
-  } else {
-    $show = show(_no_entrys_yet, array("colspan" => "4"));
-  }
+    else
+        $show = show(_no_entrys_yet, array("colspan" => "4"));
 
-  $seiten = nav($entrys,$martikel,"?page");
-  $index = show($dir."/artikel", array("show" => $show,
-                                       "stats" => $stats,
-                                       "nav" => $seiten,
-                                       "artikel" => _artikel,
-                                       "datum" => _datum,
-                                       "autor" => _autor,
-                                       "archiv" => _news_archiv));
+    $seiten = nav($entrys,$martikel,"?page");
+    $index = show($dir."/artikel", array("show" => $show, "nav" => $seiten));
 }
 ?>
