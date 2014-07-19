5041acc3aab53363c13f550716e0da1980f57e56
Lucas <godkiller_nt@hammermaps.de>
2014-07-19 22:43:08 +0200

    Lizenzbestimmungen UML Fix

--- _installer/index/default.php
+++ _installer/index/default.php
@@ -7,4 +7,4 @@ setcookie('agb',false);
 if(!is_php('5.3.0'))
     $index = writemsg(php_version_error,true);
 else
-    $index = show("welcome",array("lizenz" => file_get_contents(basePath.'/_installer/system/lizenz.txt'))); //Willkommen & AGB
+    $index = show("welcome",array("lizenz" => convert::ToHTML(file_get_contents(basePath.'/_installer/system/lizenz.txt')))); //Willkommen & AGB
